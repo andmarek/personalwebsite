6871290d8c02f8783c9018a3a7a237ada34930e5
andmarek <andmarek@outlook.com>
2019-05-23 11:01:32 -0500

    jquery scrolling

---
+++ main.js
@@ -0,0 +1,16 @@
+//jQuery Smooth Scroll
+$('.nav a').on('click', function(e) {
+  const hash = this.hash;
+  console.log(hash);
+  if(this.hash != '') {
+    e.preventDefault();
+
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      }, 
+      800
+    );
+  }
+});
